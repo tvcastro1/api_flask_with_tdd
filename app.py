b469fdf53a24d8e7319efc9ea483a6de2ff6afda
Thiago Castro <thiago.vcastro@mrv.com.br>
2023-08-09 20:04:45 -0300

    implementação app (main)

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_restful import Api
+
+from purchase_orders.resources import PurchaseOrders
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+    
+api.add_resource(PurchaseOrders, '/purchase_orders')
+app.run(port=5000)
